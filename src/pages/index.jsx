c33601328c61c455a1b627760ca3c3974062bfd1
andersonstudioa <anderson@studioaweb.com.br>
2022-11-24 18:34:35 -0300

    Inserção de título na Home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 export default function Index() {
     return (
       <p id="zero-state">
+        <h2>Clique no botão "Novo" para adicionar um contato</h2>
         Esta é uma aplicação de demonstração do React Router.
         <br />
         Confira a documentação em {" "}
